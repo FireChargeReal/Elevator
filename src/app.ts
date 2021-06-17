cf788f8a11f89cc5f636616f84fe47326fcec970
FireChargeReal <48412324+FireChargeReal@users.noreply.github.com>
2021-06-17 12:45:00 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -73,7 +73,9 @@ export default class HelloWorld {
 					}
 				},
 				collider: {
-					geometry: { shape: MRE.ColliderType.Auto },
+					geometry: { shape: MRE.ColliderType.Box,
+					size: {x:0.5 ,y:0.5 ,z:0.5}
+				 },
 					layer: MRE.CollisionLayer.Navigation
 				}
 			}
