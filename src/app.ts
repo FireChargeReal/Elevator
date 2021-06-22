7d45610c4575ac5456c90ff4a064a875801cde82
FireChargeReal <48412324+FireChargeReal@users.noreply.github.com>
2021-06-23 01:38:47 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -54,7 +54,7 @@ export default class Elevator {
 					local: {
 						position: { x: -1 + this.config["x"], y: 0 + this.config["y"], z: 0.4 + this.config["z"] },
 						scale: { x: 0.4, y: 0.4, z: 0.4 },
-						rotation: { x: 0, y: 180, z: 0 }
+						rotation: { x: 0, y: 0, z: 0 }
 					}
 				}
 			}
